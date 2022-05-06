ab6f061b01e97cd0f564a0c7f4538fe5114bef70
omarzouaoui <zouaouiomar80@gmail.com>
2022-05-06 13:07:08 +0200

    kamel ya houssem

--- CCQ1.js
+++ CCQ1.js
@@ -1,26 +1,27 @@
 var countries=[
-    {name:"Tunisia",Capital:"Tunis",Random:[]},
-    {name:"Mauritania",Capital:"Nouakchot",Random:[]},
-    {name:"Morocco",Capital:"Rabat",Random:[]},
-    {name:"Algeria",Capital:"Algiers",Random:[]},
-    {name:"Libya",Capital:"Tripoli",Random:[]},
-    {name:"Egypt",Capital:"Cairo",Random:[]},
-    {name:"Sudan",Capital:"Khartoum",Random:[]},
-    {name:"Djibouti",Capital:"Djibouti",Random:[]},
-    {name:"Soumalia",Capital:"Magadishu",Random:[]},
-    {name:"Comoros",Capital:"Moroni",Random:[]},
-    {name:"Palestine",Capital:"Al Qudes",Random:[]},
-    {name:"UAE",Capital:"Abu Dhabi",Random:[]},
-    {name:"Jordan",Capital:"Amman",Random:[]},
-    {name:"Irak",Capital:"Baghdad",Random:[]},
-    {name:"Lebanon",Capital:"Beirut",Random:[]},
-    {name:"Syria",Capital:"Damascus",Random:[]},
-    {name:"Qatar",Capital:"Doha",Random:[]},
-    {name:"Kuwait",Capital:"Kuwait City",Random:[]},
-    {name:"Bahrain",Capital:"Manama",Random:["Manama","Doha","Cairo","Djiouti"]},
+    {name:"Tunisia",Capital:"Tunis",Random:["Tunis","Noukchot","Tripoli","Khortoum"]},
+    {name:"Mauritania",Capital:"Nouakchot",Random:["Cairo","Magadishu","Khartoum","Nouakchot"]},
+    {name:"Morocco",Capital:"Rabat",Random:["Sanaa","Djibouti","Rabat","Cairo"]},
+    {name:"Algeria",Capital:"Algiers",Random:["Doha","Amman","Algiers","ABu Dhabi"]},
+    {name:"Libya",Capital:"Tripoli",Random:["Tripoli","Manama","Sanaa","Damascus"]},
+    {name:"Egypt",Capital:"Cairo",Random:["Riadh","Cairo","Beirut","Doha"]},
+    {name:"Sudan",Capital:"Khartoum",Random:["Khartoum","Magadishu","Hargesia","Moroni"]},
+    {name:"Djibouti",Capital:"Djibouti",Random:["Djibouti","Magadishu","Hargesia","Damascus"]},
+    {name:"Soumalia",Capital:"Magadishu",Random:["Magadishu","Moroni","Hargesia","Manama"]},
+    {name:"Comoros",Capital:"Moroni",Random:["Amman","Magadishu","Kuwait City","Moroni"]},
+    {name:"Palestine",Capital:"Al Qudes",Random:["Al Qudes","Al Qudes","Al Qudes","Al Qudes"]},
+    {name:"UAE",Capital:"Abu Dhabi",Random:["Riadh","Baghdad","Doha","Abu Dhabi"]},
+    {name:"Jordan",Capital:"Amman",Random:["Tripoli","Manama","Amman","Damascus"]},
+    {name:"Irak",Capital:"Baghdad",Random:["Baghdad","Damascus","Doha","Sanaa"]},
+    {name:"Lebanon",Capital:"Beirut",Random:["Riadh","Algiers","Moroni","Beirut"]},
+    {name:"Syria",Capital:"Damascus",Random:["Tunis","Amman","Damascus","Manama"]},
+    {name:"Qatar",Capital:"Doha",Random:["Baghdad","Doha","Sanaa","Riadh"]},
+    {name:"Kuwait",Capital:"Kuwait City",Random:["Kuwait City","Cairo","Nouakchot","Rabat"]},
+    {name:"Bahrain",Capital:"Manama",Random:["Tunis","Manama","Doha","Cairo","Djiouti"]},
     {name:"Saudi Arabia",Capital:"Riadh",Random:["Tunis","Abu Dhabi","Baghdad","Riadh"]},
     {name:"Yamen",Capital:"Sanaa",Random:["Sanaa","Amman","Rabat","Manama"]},
     {name:"Somalialand",Capital:"Hargesia",Random:["Magadishu","Damascus","Hargesia","Nouakchot"]},
+    {name:"Oman",Capital:"Mascate",Random:["Cairo","Mascate","Damascus","Baghdad"]}
 ]
 var randomCountries = function(array){
 var randomIndex = Math.floor(Math.random() * array.length);
@@ -31,10 +32,73 @@ var randomIndex = Math.floor(Math.random() * array.length);
 return array[randomIndex].name;
 };
 var randomCapital = function(array){
-var randomIndex = Math.floor(Math.random() * array.length);
-return array[randomIndex].Capital;
-};
+    var randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex].Capital;
+    };
+/*var randomCapital = function(array){
+    var randomIndex=[]
+    for(i=0;i<3;i++){
+        randomIndex.push(Math.floor(Math.random() * array.length))};
+        console.log(randomIndex)
+        for(i=0;i<array.length;i++){
+            var x=array[randomIndex];
+        }
+        return x
+    }*/
 var random = function(array){
 var randomIndex = Math.floor(Math.random() * array.length);
 return array[randomIndex].Random;
 };
+
+//var x=document.getElementById('a')
+//x=function randomCapital(countries)
+//countries r
+var run=randomCountries(countries)
+//rand name
+$('#country').append(run.name)
+//Rund of countries
+$('#a').append(run.Random[0])
+$('#b').append(run.Random[1])
+$('#c').append(run.Random[2])
+$('#d').append(run.Random[3])
+
+//Click
+var counter=0
+$('#a').click(function(){
+    if(`${run.Random[0]}`===`${run.Capital}`){
+        $('#comment').append('Correct , good job!')
+        $('#comment').css('color','green')
+    } else{
+        $('#comment').append('Sorry , but Nope')
+        $('#comment').css('color','red')
+    }
+})
+$('#b').click(function(){
+    if(`${run.Random[1]}`===`${run.Capital}`){
+        $('#comment').append('Correct , good job!')
+        $('#comment').css('color','green')
+    } else{
+        $('#comment').append('Sorry , but Nope')
+        $('#comment').css('color','red')
+    }
+})
+$('#c').click(function(){
+    if(`${run.Random[2]}`===`${run.Capital}`){
+        $('#comment').append('Correct , good job!')
+        $('#comment').css('color','green')
+    } else{
+        $('#comment').append('Sorry , but Nope')
+        $('#comment').css('color','red')
+    }
+})
+$('#d').click(function(){
+    if(`${run.Random[3]}`===`${run.Capital}`){
+        $('#comment').append('Correct , good job!')
+        $('#comment').css('color','green')
+    } else{
+        $('#comment').append('Sorry , but Nope')
+        $('#comment').css('color','red')
+    }
+})
+
+//reapeted f
